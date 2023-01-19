a7171e067d1c3eb75f793c4534038abc2efb9768
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-19 17:41:02 -0600

    Update Form.php

--- Block/Form.php
+++ Block/Form.php
@@ -2,5 +2,19 @@
 namespace TresdTech\FinalProject\Block;
 class Form extends \Magento\Framework\View\Element\Template
 {
+    protected $_postFactory;
+	public function __construct(
+		\Magento\Framework\View\Element\Template\Context $context,
+		\TresdTech\FinalProject\Model\PostFactory $postFactory
+	)
+	{
+		$this->_postFactory = $postFactory;
+		parent::__construct($context);
+	}
+
+	public function crud()
+	{
+		return __('ESTE ES EL Form');
+	}
 
 }
