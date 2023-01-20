2129524b82ef9681213b3fd317a256b08638495d
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-20 14:11:22 -0600

    Update Submit.php

--- Controller/Index/Submit.php
+++ Controller/Index/Submit.php
@@ -2,25 +2,22 @@
  
 namespace TresdTech\FinalProject\Controller\Index;
  
-use \Magento\Framework\App\Action\Context;
 use \Magento\Framework\View\Result\PageFactory;
-use \TresdTech\FinalProject\Model\ExtensionFactory;
 use \Magento\Framework\Controller\ResultFactory;
-use \Magento\Framework\App\Action\Action;
  
-class Submit extends Action
+class Submit extends \Magento\Framework\App\Action\Action
 {
-    protected $resultPageFactory;
-    protected $extensionFactory;
+    protected $_resultPageFactory;
+    protected $_extensionFactory;
  
     public function __construct(
-        Context $context,
-        PageFactory $resultPageFactory,
-        ExtensionFactory $extensionFactory
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \TresdTech\FinalProject\Model\ExtensionFactory ExtensionFactory $extensionFactory
     )
     {
-        $this->resultPageFactory = $resultPageFactory;
-        $this->extensionFactory = $extensionFactory;
+        $this->_resultPageFactory = $resultPageFactory;
+		$this->_extensionFactory = $extensionFactory;
         $model = $this->_FormModel->create();
         parent::__construct($context);
     }
@@ -30,14 +27,14 @@ class Submit extends Action
         try {
             $data = (array)$this->getRequest()->getPost();
             if ($data) {
-                $model = $this->extensionFactory->create();
+                $model = $this->_extensionFactory->create();
                 $model->setData($data)->save();
                 $this->messageManager->addSuccessMessage(__("Data Saved Successfully."));
             }
         } catch (\Exception $e) {
             $this->messageManager->addErrorMessage($e, __("We can\'t submit your request, Please try again."));
         }
-        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        $resultRedirect = $this->_resultFactory->create(ResultFactory::TYPE_REDIRECT);
         $resultRedirect->setUrl($this->_redirect->getRefererUrl());
         return $resultRedirect;
  
