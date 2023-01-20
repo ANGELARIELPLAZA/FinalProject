94ea8611e0f0478c492108551dc9387c9c3cedee
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-20 12:55:28 -0600

    Update Insertdata.php

--- Controller/Form/Insertdata.php
+++ Controller/Form/Insertdata.php
@@ -1,22 +1,41 @@
 <?php
 namespace TresdTech\FinalProject\Controller\Form;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Meetanshi\Extension\Model\ExtensionFactory;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\App\Action\Action;
 class Insertdata extends \Magento\Framework\App\Action\Action
 {
-    public function __construct(\Magento\Framework\App\Action\Context $context)
+    protected $resultPageFactory;
+    protected $extensionFactory;
+ 
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+        ExtensionFactory $extensionFactory
+    )
     {
-        return parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+        $this->extensionFactory = $extensionFactory;
+        parent::__construct($context);
     }
  
     public function execute()
     {
-        $post = $this->getRequest()->getPost();
-        print_r($post);
-        if ($post) {
-                //Your Code To perform Action On Post Data
-                //To Display Data
-                //To Insert Data In Database
-                echo "Hello World";
-		exit;
+        try {
+            $data = (array)$this->getRequest()->getPost();
+            if ($data) {
+                $model = $this->extensionFactory->create();
+                $model->setData($data)->save();
+                $this->messageManager->addSuccessMessage(__("Data Saved Successfully."));
+            }
+        } catch (\Exception $e) {
+            $this->messageManager->addErrorMessage($e, __("We can\'t submit your request, Please try again."));
         }
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        $resultRedirect->setUrl($this->_redirect->getRefererUrl());
+        return $resultRedirect;
+ 
     }
 }
