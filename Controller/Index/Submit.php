fa65cc61131296d0ce9f203bbfe6d492ddd8ac4f
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-20 14:17:24 -0600

    Update Submit.php

--- Controller/Index/Submit.php
+++ Controller/Index/Submit.php
@@ -1,41 +1,21 @@
 <?php
- 
 namespace TresdTech\FinalProject\Controller\Index;
- 
-use \Magento\Framework\View\Result\PageFactory;
-use \Magento\Framework\Controller\ResultFactory;
- 
+
 class Submit extends \Magento\Framework\App\Action\Action
 {
-    protected $_resultPageFactory;
-    protected $_extensionFactory;
- 
-    public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
-        \TresdTech\FinalProject\Model\ExtensionFactory ExtensionFactory $extensionFactory
-    )
-    {
-        $this->_resultPageFactory = $resultPageFactory;
-		$this->_extensionFactory = $extensionFactory;
-        parent::__construct($context);
-    }
- 
-    public function execute()
-    {
-        try {
-            $data = (array)$this->getRequest()->getPost();
-            if ($data) {
-                $model = $this->_extensionFactory->create();
-                $model->setData($data)->save();
-                $this->messageManager->addSuccessMessage(__("Data Saved Successfully."));
-            }
-        } catch (\Exception $e) {
-            $this->messageManager->addErrorMessage($e, __("We can\'t submit your request, Please try again."));
-        }
-        $resultRedirect = $this->_resultFactory->create(ResultFactory::TYPE_REDIRECT);
-        $resultRedirect->setUrl($this->_redirect->getRefererUrl());
-        return $resultRedirect;
- 
-    }
+	protected $_pageFactory;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+
+	public function submit()
+	{
+		echo "Hello World";
+		exit;
+	}
 }
