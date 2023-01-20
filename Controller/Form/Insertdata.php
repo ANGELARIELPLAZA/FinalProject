f4526880f64cd30e619016b0ba320bacaebd6818
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-20 12:16:43 -0600

    update

---
+++ Controller/Form/Insertdata.php
@@ -0,0 +1,20 @@
+<?php
+namespace TresdTech\FinalProject\Controller\Index;
+class Products extends \Magento\Framework\App\Action\Action
+{
+    public function __construct(\Magento\Framework\App\Action\Context $context)
+    {
+        return parent::__construct($context);
+    }
+ 
+    public function execute()
+    {
+        $post = $this->getRequest()->getPost();
+        print($post)
+        if ($post) {
+                //Your Code To perform Action On Post Data
+                //To Display Data
+                //To Insert Data In Database
+        }
+    }
+}
