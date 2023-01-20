682cfaecf9f50e6ee052ff87dffedc2fa3119241
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-20 13:26:06 -0600

    Update Form.php

--- Block/Form.php
+++ Block/Form.php
@@ -2,18 +2,15 @@
 
 namespace TresdTech\FinalProject\Block;
 
-use Magento\Framework\View\Element\Template;
-use Magento\Backend\Block\Template\Context;
-
-class Form extends Template
+class Form extends \Magento\Framework\View\Element\Template
 {
-    public function __construct(Context $context, array $data = [])
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context, array $data = [])
     {
         parent::__construct($context, $data);
     }
 
     public function getFormAction()
     {
-        return $this->getUrl('extension/index/submit', ['_secure' => true]);
+        return $this->getUrl('finalproject/index/submit', ['_secure' => true]);
     }
 }
