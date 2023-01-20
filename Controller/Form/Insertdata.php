b97723d7ffdc966ac95f7851f3eb230f225fb190
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-20 12:20:10 -0600

    Update Insertdata.php

--- Controller/Form/Insertdata.php
+++ Controller/Form/Insertdata.php
@@ -10,11 +10,13 @@ class Products extends \Magento\Framework\App\Action\Action
     public function execute()
     {
         $post = $this->getRequest()->getPost();
-        print($post)
+        print($post);
         if ($post) {
                 //Your Code To perform Action On Post Data
                 //To Display Data
                 //To Insert Data In Database
+                echo "Hello World";
+		exit;
         }
     }
 }
