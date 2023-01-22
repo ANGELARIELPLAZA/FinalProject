b56e81f50e8b481c5c230bb5ce591acc744e981d
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-22 16:43:43 -0600

    Update Index.php

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -22,6 +22,7 @@ class Index extends \Magento\Framework\App\Action\Action
 	{
 		$post = $this->_postFactory->create();
 		$collection = $post->getCollection();
+		if ($servicesImpacted->where('name', $service->name)->count() === 0)
 		foreach($collection as $item){
 			echo "<pre>";
 			print_r($item->getData());
