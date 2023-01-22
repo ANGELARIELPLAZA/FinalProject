e313674b8449ca694ec44660869eb80822dc9850
ANGEL ARIEL <ielc19.aplazaa@itesco.edu.mx>
2023-01-22 17:08:20 -0600

    Update Index.php

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -25,11 +25,7 @@ class Index extends \Magento\Framework\App\Action\Action
 		
 		foreach($collection as $item){
 			echo "<pre>";
-			if ($item < 1){
-				echo "no data";
-		}else{
-			print_r($item->getData());
-		}
+			print_r($item);
 			echo "</pre>";
 		}
 		exit();
